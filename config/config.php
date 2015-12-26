02fdc2497aeb778929e129f076e69bb571fc2904
Enzo <Nyzo@users.noreply.github.com>
2015-12-26 12:18:07 +0100

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+    "db_host" => "localhost",
+	"db_name" => "",
+    "db_user" => "",
+	"db_pass" => "",
+	"fb_app_id" => "",
+	"fb_app_secret" => "",
+	"fb_page_id" => "",
+	"fb_access_token" => "",
+	"tw_consumer_key" => "",
+	"tw_consumer_secret" => "",
+	"tw_access_token" => "",
+	"tw_access_token_secret" => "",
+	"gp_page_id" => ""
+);
+
+//Tout ce qui est de fb_page_id, etc n'est pas obligatoire
