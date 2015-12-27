7238039001574be666f94a642cd9dd80479164f6
Enzo <Nyzo@users.noreply.github.com>
2015-12-27 18:00:47 +0100

    Update Entity.php

--- core/Entity/Entity.php
+++ core/Entity/Entity.php
@@ -4,6 +4,12 @@ namespace Core\Entity;
 
 class Entity {
 	
+	protected $page = PATH_USER;
+	
+	public function __construct(){
+		$this->page = $_SERVER['REQUEST_URI'];
+	}
+	
 	public function __get($key){
 		$method = 'get' . ucfirst($key);
 		$this->$key = $this->$method();
