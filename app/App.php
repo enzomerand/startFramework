45df5ec3b270671bfedf61e57be298131ef0fe84
Enzo <enzo7337@gmail.com>
2016-01-13 12:30:53 +0100

    move config

--- app/App.php
+++ app/App.php
@@ -54,7 +54,7 @@ class App{
 	}
 	
 	public function getDb(){
-		$config = Config::getInstance(ROOT . '/config/config.php');
+		$config = Config::getInstance(ROOT . '/app/config.php');
 		if(is_null($this->db_instance))
 			$this->db_instance = new Database($config->get('db_host'), $config->get('db_name'), $config->get('db_user'), $config->get('db_pass'));
 		
