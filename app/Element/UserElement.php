b3178c8cc41900e3d75a2cfa5f76178feb6a02a5
Enzo <Nyzo@users.noreply.github.com>
2016-01-19 22:33:57 +0100

    Update UserElement.php

--- app/Element/UserElement.php
+++ app/Element/UserElement.php
@@ -7,18 +7,9 @@ use Core\Element\Element;
 class UserElement extends Element {
 	
 	private $page = PATH_USER;
-	protected $user_id;
 	
 	public $isAdmin = false;
 	public $isArtist = false;
-	
-	public function setId($id){
-		$this->user_id = $id;
-	}
-	
-	public function getId(){
-		return $this->user_id;
-	}
 	
 	public function setLevel(){
 		switch($this->getLevel()->user_level){
@@ -35,12 +26,12 @@ class UserElement extends Element {
 			INNER JOIN " . PREFIX . "users_level
 			    ON user_level = level_id
 			WHERE  user_id = ?
-		", [$this->user_id], true);
+		", [user_id], true);
 	}
 	
 	public function findUser($id = null){
 		unset($id); //on laisse le id en paramètre pour compatibilité avec la classe parente
-		return $this->query("SELECT user_id, user_email, user_date_create, user_key, user_phone, user_account_activate FROM " . PREFIX . "users WHERE user_id = ?", [$this->user_id], true);
+		return $this->query("SELECT user_id, user_email, user_date_create, user_key, user_phone, user_account_activate, user_level FROM " . PREFIX . "users WHERE user_id = ?", [user_id], true);
 	}
 	
 }
