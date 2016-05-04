81e19f321b33d864eb9e63d7915ded231d110fd5
Enzo <Nyzo@users.noreply.github.com>
2016-05-04 22:22:17 +0200

    Update Form.php

--- core/Form/Form.php
+++ core/Form/Form.php
@@ -13,10 +13,12 @@ class Form {
 		$this->reCaptcha = new \ReCaptcha\ReCaptcha(CAPTCHA_PRIVATE);
 	}
 	
-	protected function surround($html, $label, $class = false){
+	protected function surround($html, $label, $class = false, $surround = false){
 		$class = ($class) ? 'class="' . $class . '"' : null;
-		
-		return "<{$this->surround}{$class}>{$label}{$html}</{$this->surround}>";
+		if($surround == true)
+		    return "<{$this->surround}{$class}>{$label}{$html}</{$this->surround}>";
+		else
+			return $label . $html;
 	}
 	
 	protected function getValue($index){
@@ -26,22 +28,26 @@ class Form {
 	public function input($name = null, $label = null, $options = []){
 		$required = !empty($options['required']) ? ' required=""' : null;
 		$disabled = !empty($options['disabled']) ? ' disabled' : null;
-		$value = empty($options['value']) ? ' value="' . $this->getValue($name) . '"' : (($options['value'] === false) ? null : ' value="' . $options['value'] . '"');
+		$value = ($options['value'] == true || empty($options['value'])) ? ' value="' . $this->getValue($name) . '"' : (($options['value'] === false) ? null : ' value="' . $options['value'] . '"');
 		$type = !empty($options['type']) ? 'type="' . $options['type'] . '"' : 'type="text"';
 		$placeholder = isset($options['placeholder']) ? " placeholder=\"{$options['placeholder']}\"" : null;
 		$id = isset($options['id']) ? " id=\"{$options['id']}\"" : null;
 		$label = (!empty($label)) ? "<label>{$label}</label>" : null;
-		return $this->surround('<input ' . $type . ' name="' . $name . '" ' . $value . $placeholder . $id . $disabled . $required . ' />', $label);
+		$class = !empty($options['class']) ? " class=\"{$options['class']}\"" : null;
+		$surround = !empty($options['surround']) ? $options['surround'] : false;
+		
+		return $this->surround('<input ' . $type . ' name="' . $name . '" ' . $value . $placeholder . $id . $disabled . $required . $class . ' />', $label, false, $surround);
 	}
 	
 	public function textarea($name = null, $label = null, $options = []){
 		$label = !empty($label) ? "<label>{$label}</label>" : null;
 		$name_b = !empty($name) ? 'name="' . $name . '"' : null;
 		$placeholder = !empty($options['placeholder']) ? " placeholder=\"{$options['placeholder']}\"" : null;
-		$value = empty($options['value']) ? $this->getValue($name) : (($options['value'] === false) ? null : $options['value']);
+		$value = ($options['value'] == true || empty($options['value'])) ? $this->getValue($name) : (($options['value'] === false) ? null : $options['value']);
 		$rows = !empty($options['rows']) ? 'rows="' . $options['rows'] . '"' : null;
+		$class = !empty($options['class']) ? "class=\"{$options['class']}\"" : null;
 		
-		return $label . "<textarea spellcheck=\"true\" {$name_b} {$placeholder} {$rows}>{$value}</textarea>";
+		return $label . "<textarea spellcheck=\"true\" {$name_b} {$placeholder} {$rows} {$class}>{$value}</textarea>";
 	}
 	
 	public function captcha($public_key = null){
